15d84fc399a0f8d4c5a5b57a90836284ac6f1e4c
Craig Thacker <craigthackerx@gmail.com>
2022-02-04 01:26:03 +0000

    update name

--- dns/craigthackerdotcloud/dns.tf
+++ dns/craigthackerdotcloud/dns.tf
@@ -2,10 +2,16 @@ resource "digitalocean_domain" "default" {
   name = "craigthacker.cloud"
 }
 
-# Add an A record to the domain for www.example.com.
 resource "digitalocean_record" "dev" {
   domain = digitalocean_domain.default.id
   type   = "CNAME"
   name   = "www"
   value  = "craigthacker.dev"
+}
+
+resource "digitalocean_record" "a" {
+  domain = digitalocean_domain.default.id
+  type   = "CNAME"
+  name   = "www"
+  value  = "185.199.111.153"
 }
